1365eb3ca145249529692b4efe03cfc8f28dc93b
Your Name <you@example.com>
2022-08-05 00:37:05 -0400

    FEAT add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import { LoginPageComponent, UserListPageComponent } from "./pages";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<LoginPageComponent />} />
+        <Route path="/users" element={<UserListPageComponent />} />
+
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
