6f83e60198c91d812c2d55371f61d42cc942d09f
Your Name <you@example.com>
2022-08-14 03:00:02 -0400

    STYLE add responsiveness to login page

--- src/pages/login/login-page.component.tsx
+++ src/pages/login/login-page.component.tsx
@@ -1,16 +1,25 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
+import { Grid } from '@mui/material';
 import React from 'react';
 
 import { LoginFormComponent } from '../../components';
 
 function LoginPageComponent() {
   return (
-    <div>
-      <img src="" alt="" />
+    <Grid
+      justifyContent="center"
+      alignContent="center"
+      minWidth="100px"
+      mx={{
+        xl: 50, lg: 40, md: 30, sm: 20, xs: 10,
+      }}
+      my={20}
+    >
       <LoginFormComponent />
-    </div>
+    </Grid>
   );
 }
+// 375px x 667px
 
 export default LoginPageComponent;
