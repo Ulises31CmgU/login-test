dbdfb7524dbb414c5e33e425cfb98ab1da5c14c5
Your Name <you@example.com>
2022-08-14 17:23:45 -0400

    FEAT STYLE use new select component and add style

--- src/pages/user-list/user-list-page.component.tsx
+++ src/pages/user-list/user-list-page.component.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import React, { Suspense, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  Fab, Grid, Tooltip,
+} from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 import { requestUsersStart } from '../../app/slices/user-list.slice';
 import { selectUsersData, selectUsersDataCount } from '../../app/selectors/user-list.selectors';
 
-import { CardPreviewComponent, PaginationComponent } from '../../components';
+import { CardPreviewComponent, CustomSelectComponent, PaginationComponent } from '../../components';
 import { logout } from '../../app/slices/user.slice';
 
 function UserListPageComponent() {
@@ -17,39 +20,56 @@ function UserListPageComponent() {
   const userListCount = useSelector(selectUsersDataCount);
 
   const [currentPage, setCurrentPage] = useState(1);
-  const [dataPerPage, setDataPerPage] = useState(6);
+  const [dataPerPage, setDataPerPage] = useState(4);
+  const userDisplayOptions = [1, 2, 3, 4, 5, 6];
+  const handleSelectChange = (event:any) => {
+    setDataPerPage(event.target.value);
+  };
 
   useEffect(() => {
     dispatch(requestUsersStart(dataPerPage, currentPage));
   }, [currentPage, dataPerPage]);
 
   return (
-    <Suspense>
-
-      <div>
-        <select
-          value={dataPerPage}
-          onChange={(event) => (setDataPerPage(parseInt(event.target.value, 10)))}
-        >
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={4}>4</option>
-          <option value={5}>5</option>
-          <option value={6}>6</option>
-        </select>
-        <button type="button" onClick={() => dispatch(logout())}>
-          log out
-        </button>
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      spacing={3}
+      my={5}
+    >
+      <Grid item xs={12}>
         <CardPreviewComponent data={userList} />
-        <PaginationComponent
-          totalDataCount={userListCount}
-          dataCountPerPage={dataPerPage}
-          handlePageChange={setCurrentPage}
-          currentPage={currentPage}
-        />
-      </div>
-    </Suspense>
+      </Grid>
+
+      <Grid container item justifyContent="center">
+        <Grid item>
+          <CustomSelectComponent
+            value={dataPerPage}
+            handleChange={handleSelectChange}
+            displayOptions={userDisplayOptions}
+            helpText="Amount of users to display"
+          />
+        </Grid>
+        <Grid item>
+          <PaginationComponent
+            totalDataCount={userListCount}
+            dataCountPerPage={dataPerPage}
+            handlePageChange={setCurrentPage}
+            currentPage={currentPage}
+          />
+        </Grid>
+      </Grid>
+      <Grid item alignSelf="flex-end" mr={2}>
+        <Tooltip title="Log out" placement="top-start" arrow>
+
+          <Fab color="error" onClick={() => dispatch(logout())}>
+            <LogoutIcon />
+          </Fab>
+        </Tooltip>
+      </Grid>
+    </Grid>
   );
 }
 
