31d4c67d7cfec9aaf6e2e03d04a4214a14173a5a
Your Name <you@example.com>
2022-08-14 01:00:25 -0400

    ADD content to user-list page component

--- src/pages/user-list/user-list-page.component.tsx
+++ src/pages/user-list/user-list-page.component.tsx
@@ -1,14 +1,56 @@
-import React from 'react'
-import { connect } from 'react-redux'
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import React, { Suspense, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// eslint-disable-next-line import/no-extraneous-dependencies
 
-export const UserListPageComponent = (props: any) => {
+import { requestUsersStart } from '../../app/slices/user-list.slice';
+import { selectUsersData, selectUsersDataCount } from '../../app/selectors/user-list.selectors';
+
+import { CardPreviewComponent, PaginationComponent } from '../../components';
+import { logout } from '../../app/slices/user.slice';
+
+function UserListPageComponent() {
+  const dispatch = useDispatch();
+
+  const userList = useSelector(selectUsersData);
+  const userListCount = useSelector(selectUsersDataCount);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [dataPerPage, setDataPerPage] = useState(6);
+
+  useEffect(() => {
+    dispatch(requestUsersStart(dataPerPage, currentPage));
+  }, [currentPage, dataPerPage]);
+
   return (
-    <div>UserListPageComponent</div>
-  )
+    <Suspense>
+
+      <div>
+        <select
+          value={dataPerPage}
+          onChange={(event) => (setDataPerPage(parseInt(event.target.value, 10)))}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+          <option value={6}>6</option>
+        </select>
+        <button type="button" onClick={() => dispatch(logout())}>
+          log out
+        </button>
+        <CardPreviewComponent data={userList} />
+        <PaginationComponent
+          totalDataCount={userListCount}
+          dataCountPerPage={dataPerPage}
+          handlePageChange={setCurrentPage}
+          currentPage={currentPage}
+        />
+      </div>
+    </Suspense>
+  );
 }
 
-const mapStateToProps = (state: any) => ({})
-
-const mapDispatchToProps = {}
-
-export default connect(mapStateToProps, mapDispatchToProps)(UserListPageComponent)
+export default UserListPageComponent;
