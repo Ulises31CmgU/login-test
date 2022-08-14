ee1d56c882f06b3fe30a3ca276709ba92803d752
Your Name <you@example.com>
2022-08-14 01:03:36 -0400

    CREATE index file for components module

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-unresolved */
+export { default as CustomTextInputComponent } from './custom-input-field/custom-input-field.component';
+export { default as SpinnerComponent } from './spinner/spinner.component';
+export { default as CardComponent } from './card/card.component';
+export { default as CardPreviewComponent } from './card-preview/card-preview.component';
+export { default as LoginFormComponent } from './login-form/login-form.component';
+export { default as PaginationComponent } from './pagination/pagination.component';
+export { default as SnackbarComponent } from './snackbar/snackbar.component';
